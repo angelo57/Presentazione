bb5015297f077f8497fddc99a5413a71e38d5e56
Angelo Messina <angelomessina@iMac-di-Angelo.local>
2012-09-24 15:06:43 +0200

    Initial Commit

---
+++ Presentazione/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Presentazione
+//
+//  Created by Angelo Messina on 24/09/12.
+//  Copyright (c) 2012 com.angelomessina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
